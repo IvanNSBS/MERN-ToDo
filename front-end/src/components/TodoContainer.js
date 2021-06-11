1a8e2ffd8db1f1c98bb125643740e1050f0f22fc
IvanNSBS <insbs@cin.ufpe.br>
2021-06-10 22:15:51 -0300

    Adding Day Display Component

--- front-end/src/components/TodoContainer.js
+++ front-end/src/components/TodoContainer.js
@@ -1,57 +1,17 @@
 import React from "react";
 import "../styles/style.css"
+import CurrentDayView from "./CurrentDayView";
 import ControlledTextArea from "./ControlledTextArea";
 import ControlledDropdown from "./ControlledDropdown";
 
 class TodoContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date(), condition: false };
-
-        this.toggleCondition = this.toggleCondition.bind(this);
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval( () => this.tick(), 1000 );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    toggleCondition() {
-        this.setState((state) => ({
-            condition: !state.condition
-        }));
-    }
-
     render() 
     {
-        let conditionalRender;
-        if(this.state.condition) conditionalRender = "Marrapaaaais" 
-        else conditionalRender = "";
-
         return( 
         <div id="todo-container" onClick={this.toggleCondition}>
-            <ControlledTextArea/>
-            <ControlledDropdown/>
-            {/* { this.state.condition && <ClockTest date={this.state.date}/>} */}
-        </div>)
-    }
-}
-
-class ClockTest extends React.Component {
-    render() 
-    {
-        return( 
-        <div>
-            Time: {this.props.date.toLocaleTimeString()}.
+            <div id="day-view-container">
+                <CurrentDayView />
+            </div>
         </div>)
     }
 }
