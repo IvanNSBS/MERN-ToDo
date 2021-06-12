104cd3794f61710b5be0ea8989e8b61fe0ab96b5
IvanNSBS <insbs@cin.ufpe.br>
2021-06-12 09:21:44 -0300

    Delete button will only appear if mouse is over task element

--- front-end/src/components/Task.js
+++ front-end/src/components/Task.js
@@ -6,29 +6,41 @@ class Task extends React.Component{
     constructor(props){
         super(props);
 
+        this.state = { isMouveOver: false }
+
         this.onCheckboxToggle = this.onCheckboxToggle.bind(this);
         this.onClickDelete = this.onClickDelete.bind(this);
-        this.index = this.props.index;
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+        this.onMouseLeave = this.onMouseLeave.bind(this);
     }
 
     onCheckboxToggle(event){
         const newObj = {description: this.props.description, finished: event.target.checked}
-        this.props.onTaskUpdated( newObj, this.index );
+        this.props.onTaskUpdated( newObj, this.props.index );
     }
 
     onClickDelete(event){
-        this.props.onTaskDeleted(this.index);
+        this.props.onTaskDeleted(this.props.index);
+    }
+
+    onMouseEnter(event) {
+        this.setState( { isMouveOver: true } ); 
+    }
+
+    onMouseLeave(event) {
+        this.setState( { isMouveOver: false } ); 
     }
 
     render() 
     {
+        let deleteBtnClass = this.state.isMouveOver ? "delete-task" : "invisible";
         return( 
-            <div className="task-container">
+            <div className="task-container" onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
                 <div className="task-checkbox-container">
                     <input type="checkbox" className="task-checkbox" onClick={this.onCheckboxToggle}></input>
                     <span className={`task ${this.props.finished ? "finished-task" : ""}`}>{this.props.description}</span> 
                 </div>
-                <button className="delete-task" onClick={this.onClickDelete}>
+                <button className={ deleteBtnClass } onClick={this.onClickDelete}>
                     <BsTrashFill className="trashbin"/>
                 </button>
             </div>
