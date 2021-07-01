f843f5ad1b585e9d20a52504f89c762bcfd9849f
IvanNSBS <insbs@cin.ufpe.br>
2021-07-01 15:17:59 -0300

    Updating TodoView to use Hooks

--- front-end/src/views/TodoView.js
+++ front-end/src/views/TodoView.js
@@ -1,55 +1,28 @@
-import React from "react";
 import "../styles/style.css"
+import React, { useState } from "react";
 import { DateView } from "./DateView";
 import TaskCreator from "./TaskCreator";
 import TaskList from "./TaskList";
 
-class TodoView extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { todoItems: this.props.controller.requestAllTodos() }
+/**
+ * 
+ * @param {todoController} controller - todoController object 
+ */
+function TodoView( { controller } )
+{
+    const [allTodos, setTodos] = useState( controller.requestAllTodos() )
 
-        this.addTodo = this.addTodo.bind(this);
-        this.toggleTodo = this.toggleTodo.bind(this);
-        this.deleteTodo = this.deleteTodo.bind(this);
-    }
+    function createTodo(descr) { setTodos(controller.requestAddTodo(descr)); }
+    function updateTodo(index) { setTodos(controller.requestToggleTodo(index)); }
+    function deleteTodo(index) { setTodos(controller.requestRemoveTodo(index)); }
 
-    addTodo(todoDescription) {
-        const newTodos = this.props.controller.requestAddTodo(todoDescription);
-
-        if(newTodos != null)
-            this.setState({ todoItems: newTodos });
-        else
-            console.log("Error adding todo")
-    };
-
-    toggleTodo(objIndex) {
-        const newTodos = this.props.controller.requestToggleTodo(objIndex);
-
-        if(newTodos != null)
-            this.setState({ todoItems: newTodos });
-        else
-            console.log("Error toggling todo")
-    }
-
-    deleteTodo(objIndex) {
-        const newTodos = this.props.controller.requestRemoveTodo(objIndex);
-
-        if(newTodos != null)
-            this.setState({ todoItems: newTodos });
-        else
-            console.log("Error deleting todo")
-    }
-
-    render() 
-    {
-        return( 
-        <div id="todo-container" onClick={this.toggleCondition}>
+    return ( 
+        <div id="todo-container">
             <DateView />
-            <TaskCreator submitTask = { this.addTodo }/>
-            <TaskList  todoItems={ this.state.todoItems } toggleTodo={this.toggleTodo} deleteTodo={this.deleteTodo}/>
-        </div>)
-    }
+            <TaskCreator submitTask = { createTodo }/>
+            <TaskList  todoItems={ allTodos } toggleTodo={ updateTodo } deleteTodo={ deleteTodo }/>
+        </div>
+    )
 }
 
 export default TodoView;
